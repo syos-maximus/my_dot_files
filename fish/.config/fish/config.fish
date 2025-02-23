353a6246fe009ed3689f0024872a321b4e79ae52
syos-maximus <sebastian@aerolaw.org>
2025-02-22 18:24:36 -0800

    First Commit
    This is the first commit for dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+#adding SSH agent start
+#eval (ssh-agent -c)
+#adding my own custome variables outside cachyos
+alias dot='cd ~/my_dot_files' #navigates to my git dotfiles repo
+
