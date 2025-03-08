0e0c6ea3757f2310be1a42324b0b7101af477db6
syos-maximus <sebastian@aerolaw.org>
2025-03-08 10:12:55 -0800

    Fish config updated for changing directory with yazi; hyprland updated to add opaque value for all windows

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
+# function fish_greeting
 #    # smth smth
 #end
 
@@ -13,3 +13,13 @@ alias github='ssh-add ~/.ssh/github-key' #automates the authentication of githup
 #adding enviroment variables
 #note this location is sym linked
 export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+
+#adding wrapper for yazi to change directory when browsing
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
