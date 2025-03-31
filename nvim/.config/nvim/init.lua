7cabf8f3cccb1cebb78225170dfbd5718186503c
syos-maximus <sebastian@aerolaw.org>
2025-03-30 22:25:33 -0700

    first nvim config with the following (relative line numbers, lazy plugin manager, neotree)

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,36 @@
+-- adding relative line numbers
+vim.opt.relativenumber = true
+-- adding line numbers
+vim.opt.number = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end 
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+{
+-- adding neo tree (note i took out the neo-tree code for lazy = false
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  }
+}
+})
