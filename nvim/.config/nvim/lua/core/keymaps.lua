46043a4cbcba2f91d8b95f188ac5c8d54f86e166
syos-maximus <sebastian@aerolaw.org>
2025-04-05 18:51:46 -0700

    abbreviating how to change pains in NVIM

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -4,3 +4,11 @@ vim.g.maplocalleader = ' '
 
 --setting neotree call cntrl-n
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+
+--changing how to move between pains
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
