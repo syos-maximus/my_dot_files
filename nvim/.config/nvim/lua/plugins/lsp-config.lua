a8a5959969e7d0a92fab6e28d77fd9deda8a3d2b
syos-maximus <sebastian@aerolaw.org>
2025-04-07 20:30:35 -0700

    beginning to build language server protocol for nvim

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function() 
+			require("mason-lspconfig").setup({
+			ensure_installed = {"lua_ls", "harper_ls", "hydra_lsp", "superhtml"}
+			})
+		end
+	}
+}
+
