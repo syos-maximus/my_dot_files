ee9d5dbe6f09aa8c28656ae98aec770f0d97cca5
syos-maximus <sebastian@aerolaw.org>
2025-04-11 19:35:05 -0700

    updated treesitter plugin to automtically detect and install language parsers instead of manually calling each language

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,14 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-          		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" , "python", "fish", "css"},
-          		highlight = { enable = true },
-        		indent = { enable = true },
-       	 	})
-	end
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+-- uncomment next line to not have to manually list languages for tree sitter highlighting
+            auto_install = true,
+--uncomment next line to manually call language parsers 
+--            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" , "python", "fish", "css"},
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
