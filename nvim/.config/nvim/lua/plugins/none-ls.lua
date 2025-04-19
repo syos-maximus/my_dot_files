f3dae01c47379daf3ca33df96b51d694cc07b5c7
syos-maximus <sebastian@aerolaw.org>
2025-04-18 22:08:07 -0700

    cleaning up formatters and linters

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,21 @@
 -- This plugin calls the formatters and linters
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua, --formatter for lua
-                null_ls.builtins.formatting.prettier, --formatter for javascript, css, html, yaml
-                null_ls.builtins.formatting.black, --formatter for python
-                null_ls.builtins.formatting.isort, --formatter for python importing
-                null_ls.builtins.diagnostics.pylint, --linting for python
-                null_ls.builtins.diagnostics.stylelint, --linting for css
-                null_ls.builtins.diagnostics.eslint_d, --linting for javascript
-                null_ls.builtins.diagnostics.htmlhint, --linting for html
-            },
-        })
-    end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua, --formatter for lua
+				null_ls.builtins.formatting.prettier, --formatter for javascript, css, html, yaml
+				null_ls.builtins.diagnostics.erb_lint, --linting for HTML & Ruby
+				null_ls.builtins.formatting.black, --formatter for python
+				null_ls.builtins.formatting.isort, --formatter for python importin
+				null_ls.builtins.diagnostics.pylint, --linting for python
+			},
+		})
+	end,
 }
 --make sure to use :mason to install each of the above called linters and formatters!!
