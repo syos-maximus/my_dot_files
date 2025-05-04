c91d8e5386f3ac7b46ff8e8fb5e780e7910a43e9
sebastian_ortega <sebastian@aerolaw.org>
2025-05-03 20:46:29 -0700

    Changed PDF viewer to allow real time preview and created an aux directory for auxilary files after LaTex file compile

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,10 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
         -- VimTeX configuration goes here, e.g.
-        vim.g.vimtex_view_method = "zathura"
+        --        vim.g.vimtex_view_method = "okular"
+        vim.g.vimtex_view_general_viewer = "okular"
+        vim.g.vimtex_compiler_latexmk = {
+        aux_dir = "aux", --putting all aux files into a folder after compile
+        }
     end,
 }
