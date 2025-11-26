c5bce511f7654a6bfdfdbaf1340666157db7ae30
syos-maximus <sebastian@aerolaw.org>
2025-11-25 19:57:45 -0800

    adding lua file for custom function in yazi for both file size and modification year

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,14 @@
+function Linemode:custom()
+  local year = os.date("%Y")
+  local time = (self._file.cha.mtime or 0) // 1
+
+  if time > 0 and os.date("%Y", time) == year then
+    time = os.date("%b %d %H:%M", time)
+  else
+    time = time and os.date("%b %d  %Y", time) or ""
+  end
+
+  local size = self._file:size()
+  return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
+end
+
