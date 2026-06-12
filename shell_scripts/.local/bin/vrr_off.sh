5a0712260dca0caa126a04b3e7204d86b7895f91
syos-maximus <sebastian@aerolaw.org>
2026-06-12 13:29:49 -0700

    modifications for notifications into vrr scripts

--- shell_scripts/.local/bin/vrr_off.sh
+++ shell_scripts/.local/bin/vrr_off.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hyprctl keyword monitor "DP-1,3440@164.9,0x0,1,cm,auto,vrr,0"
-
+notify-send "VRR Has Been Turned Off"
--- shell_scripts/.local/bin/vrr_on.sh
+++ shell_scripts/.local/bin/vrr_on.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 hyprctl keyword monitor "DP-1,3440x1440@164.9,0x0,1,cm,auto,vrr,2"
+notify-send "VRR is back on"
 
