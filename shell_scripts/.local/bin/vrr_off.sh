aa18fcfbe8db402434b6d1a8d9222e4556337154
syos-maximus <sebastian@aerolaw.org>
2026-09-22 22:48:26 -0700

    modifying my vrr scripts to account for new hyprland format

--- shell_scripts/.local/bin/vrr_off.sh
+++ shell_scripts/.local/bin/vrr_off.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-hyprctl keyword monitor "DP-1,3440@164.9,0x0,1,cm,auto,vrr,0"
+hyprctl eval hl.monitor({
+    output   = "DP-1",
+    mode     = "3440x1440@164.90",
+    position = "0x0",
+    scale    = 1,
+    supports_hdr = 0,
+    vrr = 0,
+})
 notify-send "VRR Has Been Turned Off"
