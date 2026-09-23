b9ea02b2cbdbdeafb2eaadb98d0f770da0aa0103
syos-maximus <sebastian@aerolaw.org>
2026-09-22 22:50:27 -0700

    modifying my vrr scripts to account for new hyprland format and adding missing quote

--- shell_scripts/.local/bin/vrr_off.sh
+++ shell_scripts/.local/bin/vrr_off.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-hyprctl eval hl.monitor({
+hyprctl eval 'hl.monitor({
     output   = "DP-1",
     mode     = "3440x1440@164.90",
     position = "0x0",
     scale    = 1,
     supports_hdr = 0,
     vrr = 0,
-})
+})'
 notify-send "VRR Has Been Turned Off"
--- shell_scripts/.local/bin/vrr_on.sh
+++ shell_scripts/.local/bin/vrr_on.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-hyprctl eval hl.monitor({
+hyprctl eval 'hl.monitor({
     output   = "DP-1",
     mode     = "3440x1440@164.90",
     position = "0x0",
     scale    = 1,
     supports_hdr = 0,
     vrr = 2,
-})
+})'
 notify-send "VRR is back on"
